650613ed734f5fd4588bdc83098451b305f2d334
Naveen <naveen.katta03@gmail.com>
2023-08-02 00:01:32 +0530

    first commit

---
+++ src/main/java/katta/Devi.java
@@ -0,0 +1,11 @@
+package katta;
+
+import org.testng.annotations.Test;
+
+public class Devi {
+	@Test
+	public static void devika() 
+	{
+		System.out.println("Devika");
+	}
+}
---
+++ src/main/java/katta/NaveenKatta.java
@@ -0,0 +1,13 @@
+package katta;
+
+import org.testng.annotations.Test;
+
+public class NaveenKatta {
+	@Test
+	public static void Naveen() 
+	{
+		System.out.println("Naveen");
+
+	}
+
+}
